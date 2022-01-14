81000dc4ff0f2b1727f2e048ba759d26b3814b69
sunila <imransunila@yahoo.com>
2022-01-14 10:58:13 -0500

    day39_recap

---
+++ src/day39_Recap/ShapeTask/Shape.java
@@ -0,0 +1,43 @@
+package day39_Recap.ShapeTask;
+
+public class Shape {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null){
+            System.out.println("name can not be null");
+            System.exit(1);// 1:some thing went wrong
+        }
+
+        if (name.isEmpty() || name.isBlank()){
+            System.out.println("invalid name");
+            System.exit(1);
+        }
+        this.name = name;
+    }
+
+
+    public Shape(String name) {
+      setName(name);
+    }
+
+
+
+
+    public double area(){
+        return 0;
+    }
+
+    public double perimeter(){
+        return 0;
+    }
+
+
+
+
+}
