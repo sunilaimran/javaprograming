76bc693fd02f47e97f6c6177d24f117ea0e31c3e
sunila <imransunila@yahoo.com>
2022-01-22 11:09:26 -0500

    day41_Exceptions

--- src/day41_Exceptions/TryCatchBlocks.java
+++ src/day41_Exceptions/TryCatchBlocks.java
@@ -1,5 +1,9 @@
 package day41_Exceptions;
 
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TryCatchBlocks {
     public static void main(String[] args) {
 
@@ -51,5 +55,39 @@ public class TryCatchBlocks {
         }
 
         System.out.println("test3 completed");
+
+
+
+        System.out.println("Hello");
+
+
+        try {
+
+            Thread.sleep(3000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Cydeo");
+
+
+        System.out.println("------------------------------------------");
+
+        try {
+            FileInputStream file = new FileInputStream("File path");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
     }
 }
